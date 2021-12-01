eef1ddca15e490d9e24613640fe6b233c1afd90f
zio7711 <zi.ao.tan@outlook.com>
2021-12-01 17:03:13 +0800

    config router file

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/pages/Home';
+import Search from '@/pages/Search';
+import Register from '@/pages/Register';
+import Login from '@/pages/Login';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+    },
+    {
+      path: '/search',
+      component: Search,
+    },
+    {
+      path: '/register',
+      component: Register,
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+  ],
+});
