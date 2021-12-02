f31cd0024982532f653cf68fea42cab6fac00f8f
zio7711 <zi.ao.tan@outlook.com>
2021-12-02 16:25:22 +0800

    create Rank component

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,17 +3,21 @@
     <TypeNav />
     <ListContainer />
     <Recommend />
+    <Rank />
   </div>
 </template>
 
 <script>
   import ListContainer from '@/pages/Home/ListContainer';
   import Recommend from '@/pages/Home/Recommend';
+  import Rank from '@/pages/Home/Rank';
+
   export default {
     name: '',
     components: {
       ListContainer,
       Recommend,
+      Rank,
     },
   };
 </script>
