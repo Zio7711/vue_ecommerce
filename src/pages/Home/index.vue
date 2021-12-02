b2d3e34c45da315b781b95ec6c0ef868c9f9e8cd
zio7711 <zi.ao.tan@outlook.com>
2021-12-02 15:58:51 +0800

    create Recommend component

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,12 +2,14 @@
   <div>
     <TypeNav />
     <ListContainer />
+    <Recommend />
   </div>
 </template>
 
 <script>
   import ListContainer from '@/pages/Home/ListContainer';
-  export default { name: '', components: { ListContainer } };
+  import Recommend from '@/pages/Home/Recommend';
+  export default { name: '', components: { ListContainer, Recommend } };
 </script>
 
 <style></style>
