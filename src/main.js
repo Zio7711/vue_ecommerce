ef442218eff59a27988ec15991edbf3b1c6357c5
zio7711 <zi.ao.tan@outlook.com>
2021-12-03 18:48:24 +0800

    remove hardcode data for categotyList

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/router';
 // global component
-import TypeNav from '@/pages/Home/TypeNav';
-import { reqCategoryList } from '@/api';
+import TypeNav from '@/components/TypeNav';
 import store from './store';
 
-reqCategoryList();
 Vue.component(TypeNav.name, TypeNav);
 
 Vue.config.productionTip = false;
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,23 @@
-const state = {};
-const mutations = {};
+import { reqCategoryList } from '@/api';
 
-const actions = {};
+const state = {
+  categoryList: [],
+};
+
+const mutations = {
+  CATEGORYLIST(state, categoryList) {
+    state.categoryList = categoryList;
+  },
+};
+
+const actions = {
+  async categoryList({ commit }) {
+    let result = await reqCategoryList();
+    if (result.code === 200) {
+      commit('CATEGORYLIST', result.data);
+    }
+  },
+};
 const getters = {};
 
 export default {
