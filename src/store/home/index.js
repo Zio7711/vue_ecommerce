fcfa67e2b961432cb7798742efd07c46052f5b77
zio7711 <zi.ao.tan@outlook.com>
2021-12-05 18:37:29 +0800

    config store floorlist

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,8 +1,9 @@
-import { reqCategoryList, reqGetBannerList } from '@/api';
+import { reqCategoryList, reqFloorList, reqGetBannerList } from '@/api';
 
 const state = {
   categoryList: [],
   bannerList: [],
+  floorList: [],
 };
 
 const mutations = {
@@ -12,6 +13,10 @@ const mutations = {
 
   BANNERLIST(state, bannerList) {
     state.bannerList = bannerList;
+  },
+
+  FLOORLIST(state, floorList) {
+    state.floorList = floorList;
   },
 };
 
@@ -27,6 +32,13 @@ const actions = {
     let result = await reqGetBannerList();
     if (result.code === 200) {
       commit('BANNERLIST', result.data);
+    }
+  },
+
+  async getFloorList({ commit }) {
+    let result = await reqFloorList();
+    if (result.code === 200) {
+      commit('FLOORLIST', result.data);
     }
   },
 };
