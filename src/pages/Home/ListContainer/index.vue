2fa51c9e23b65172c4adfac4a46d15f271d57713
zio7711 <zi.ao.tan@outlook.com>
2021-12-07 00:18:52 +0800

    refactor carousel in list container

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -3,23 +3,7 @@
     <div class="sortList clearfix">
       <div class="center">
         <!--banner轮播-->
-        <div class="swiper-container" ref="mySwiper">
-          <div class="swiper-wrapper">
-            <div
-              class="swiper-slide"
-              v-for="carousel in bannerList"
-              :key="carousel.id"
-            >
-              <img :src="carousel.imgUrl" />
-            </div>
-          </div>
-          <!-- 如果需要分页器 -->
-          <div class="swiper-pagination"></div>
-
-          <!-- 如果需要导航按钮 -->
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
-        </div>
+        <Carousel :list="bannerList" />
       </div>
       <div class="right">
         <div class="news">
@@ -97,8 +81,6 @@
 <script>
   import { mapState } from 'vuex';
 
-  import Swiper from 'swiper';
-
   export default {
     name: 'ListContainer',
 
@@ -112,27 +94,6 @@
 
     mounted() {
       this.$store.dispatch('getBannerList');
-    },
-
-    watch: {
-      bannerList: {
-        immediate: true,
-        handler() {
-          this.$nextTick(() => {
-            new Swiper(this.$refs.mySwiper, {
-              loop: true,
-              pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-              },
-              navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-              },
-            });
-          });
-        },
-      },
     },
   };
 </script>
