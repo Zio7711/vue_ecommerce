c80d9d85f51657483d083e14342d6b753a5b9c38
zio7711 <zi.ao.tan@outlook.com>
2021-12-07 00:46:58 +0800

    config search page store

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,7 +1,23 @@
-const state = {};
-const mutations = {};
+import { reqGetSearchInfo } from "@/api";
 
-const actions = {};
+const state = {
+  searchList: []
+};
+const mutations = {
+  GETSEARCHLIST(state, searchList) {
+    state.searchList = searchList
+  }
+};
+
+const actions = {
+  async getSearchList({commit}, params) {
+    let result = await reqGetSearchInfo(params)
+    ir(result.code === 200) {
+      commit('GETSEARCHLIST', result.data)
+    }
+  }
+
+};
 const getters = {};
 
 export default {
