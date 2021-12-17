6130e71336bf036d139d83fd86f52d7aff601a3d
zio7711 <zi.ao.tan@outlook.com>
2021-12-17 13:26:09 +0800

    achieve mask moving effect with mouse

--- src/pages/Detail/Zoom/Zoom.vue
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="spec-preview">
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <div class="event" @mousemove="handler"></div>
     <div class="big">
       <img :src="imgObj.imgUrl" />
     </div>
-    <div class="mask"></div>
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -17,6 +17,22 @@
       return {
         currentIndex: 0,
       };
+    },
+
+    methods: {
+      handler(event) {
+        let mask = this.$refs.mask;
+        let left = event.offsetX - mask.offsetWidth / 2;
+        let top = event.offsetY - mask.offsetHeight / 2;
+
+        if (left < 0) left = 0;
+        if (left > mask.offsetWidth) left = mask.offsetWidth;
+        if (top < 0) top = 0;
+        if (top > mask.offsetHeight) top = mask.offsetHeight;
+
+        mask.style.left = left + 'px';
+        mask.style.top = top + 'px';
+      },
     },
 
     computed: {
