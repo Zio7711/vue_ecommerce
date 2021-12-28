02b6f4c3346afe9bd2cc9720d2de2c421581b32c
zio7711 <zi.ao.tan@outlook.com>
2021-12-28 18:45:07 +0800

    fix typo in Pay index.vue component

--- src/pages/Pay/index.vue
+++ src/pages/Pay/index.vue
@@ -4,21 +4,33 @@
       <div class="checkout-tit">
         <h4 class="tit-txt">
           <span class="success-icon"></span>
-          <span class="success-info">订单提交成功，请您及时付款，以便尽快为您发货~~</span>
+          <span class="success-info"
+            >订单提交成功，请您及时付款，以便尽快为您发货~~</span
+          >
         </h4>
-        <div class="paymark">
-          <span class="fl">请您在提交订单<em class="orange time">4小时</em>之内完成支付，超时订单会自动取消。订单号：<em>145687</em></span>
-          <span class="fr"><em class="lead">应付金额：</em><em class="orange money">￥17,654</em></span>
+        <div class="payMark">
+          <span class="fl"
+            >请您在提交订单<em class="orange time">4小时</em
+            >之内完成支付，超时订单会自动取消。订单号：<em>145687</em></span
+          >
+          <span class="fr"
+            ><em class="lead">应付金额：</em
+            ><em class="orange money">￥17,654</em></span
+          >
         </div>
       </div>
       <div class="checkout-info">
         <h4>重要说明：</h4>
         <ol>
-          <li>尚品汇商城支付平台目前支持<span class="zfb">支付宝</span>支付方式。</li>
+          <li>
+            尚品汇商城支付平台目前支持<span class="zfb">支付宝</span>支付方式。
+          </li>
           <li>其它支付渠道正在调试中，敬请期待。</li>
           <li>为了保证您的购物支付流程顺利完成，请保存以下支付宝信息。</li>
         </ol>
-        <h4>支付宝账户信息：（很重要，<span class="save">请保存！！！</span>）</h4>
+        <h4>
+          支付宝账户信息：（很重要，<span class="save">请保存！！！</span>）
+        </h4>
         <ul>
           <li>支付帐号：11111111</li>
           <li>密码：111111</li>
@@ -31,48 +43,45 @@
         </div>
         <div class="step-cont">
           <ul class="payType">
-            <li><img src="./images/pay2.jpg"></li>
-            <li><img src="./images/pay3.jpg"></li>
+            <li><img src="./images/pay2.jpg" /></li>
+            <li><img src="./images/pay3.jpg" /></li>
           </ul>
-
         </div>
         <div class="hr"></div>
 
-        <div class="payshipInfo">
+        <div class="payShopInfo">
           <div class="step-tit">
             <h5>支付网银</h5>
           </div>
           <div class="step-cont">
             <ul class="payType">
-              <li><img src="./images/pay10.jpg"></li>
-              <li><img src="./images/pay11.jpg"></li>
-              <li><img src="./images/pay12.jpg"></li>
-              <li><img src="./images/pay13.jpg"></li>
-              <li><img src="./images/pay14.jpg"></li>
-              <li><img src="./images/pay15.jpg"></li>
-              <li><img src="./images/pay16.jpg"></li>
-              <li><img src="./images/pay17.jpg"></li>
-              <li><img src="./images/pay18.jpg"></li>
-              <li><img src="./images/pay19.jpg"></li>
-              <li><img src="./images/pay20.jpg"></li>
-              <li><img src="./images/pay21.jpg"></li>
-              <li><img src="./images/pay22.jpg"></li>
-
+              <li><img src="./images/pay10.jpg" /></li>
+              <li><img src="./images/pay11.jpg" /></li>
+              <li><img src="./images/pay12.jpg" /></li>
+              <li><img src="./images/pay13.jpg" /></li>
+              <li><img src="./images/pay14.jpg" /></li>
+              <li><img src="./images/pay15.jpg" /></li>
+              <li><img src="./images/pay16.jpg" /></li>
+              <li><img src="./images/pay17.jpg" /></li>
+              <li><img src="./images/pay18.jpg" /></li>
+              <li><img src="./images/pay19.jpg" /></li>
+              <li><img src="./images/pay20.jpg" /></li>
+              <li><img src="./images/pay21.jpg" /></li>
+              <li><img src="./images/pay22.jpg" /></li>
             </ul>
           </div>
-
         </div>
         <div class="hr"></div>
 
         <div class="submit">
           <router-link class="btn" to="/paysuccess">立即支付</router-link>
         </div>
-        <div class="otherpay">
+        <div class="otherPay">
           <div class="step-tit">
             <h5>其他支付方式</h5>
           </div>
           <div class="step-cont">
-            <span><a href="weixinpay.html" target="_blank">微信支付</a></span>
+            <span><a target="_blank">微信支付</a></span>
             <span>中国银联</span>
           </div>
         </div>
@@ -84,7 +93,7 @@
 <script>
   export default {
     name: 'Pay',
-  }
+  };
 </script>
 
 <style lang="less" scoped>
@@ -133,7 +142,7 @@
           }
         }
 
-        .paymark {
+        .payMark {
           overflow: hidden;
           line-height: 26px;
           text-indent: 38px;
@@ -222,7 +231,7 @@
           display: inline-block;
           padding: 15px 45px;
           margin: 15px 0 10px;
-          font: 18px "微软雅黑";
+          font: 18px '微软雅黑';
           font-weight: 700;
           border-radius: 0;
           background-color: #e1251b;
