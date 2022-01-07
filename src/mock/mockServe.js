8b15913a438c0cd269a4be224bbf2d2c28ccb423
zio7711 <zi.ao.tan@outlook.com>
2022-01-07 17:53:31 +0800

    comment the mock serve file

--- src/mock/mockServe.js
+++ src/mock/mockServe.js
@@ -1,7 +1,12 @@
+// import mockjs modules
 import Mock from 'mockjs';
+
+// import json file even tho it does not export
+// webpack is set to be exported by default: image and json file
 import banner from './banner.json';
 import floor from './floor.json';
 
+// mock data: 1. address, 2. data
 Mock.mock('/mock/banner', {
   code: 200,
   data: banner,
