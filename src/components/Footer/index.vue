296687e56866049a3f89a55ae6ad5bbe12c0c174
zio7711 <zi.ao.tan@outlook.com>
2022-01-15 18:57:18 +0800

    remove unused info in Footer

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,58 +1,6 @@
 <template>
   <div class="footer">
     <div class="footer-container">
-      <div class="footerList">
-        <div class="footerItem">
-          <h4>购物指南</h4>
-          <ul class="footerItemCon">
-            <li>购物流程</li>
-            <li>会员介绍</li>
-            <li>生活旅行/团购</li>
-            <li>常见问题</li>
-            <li>购物指南</li>
-          </ul>
-        </div>
-        <div class="footerItem">
-          <h4>配送方式</h4>
-          <ul class="footerItemCon">
-            <li>上门自提</li>
-            <li>211限时达</li>
-            <li>配送服务查询</li>
-            <li>配送费收取标准</li>
-            <li>海外配送</li>
-          </ul>
-        </div>
-        <div class="footerItem">
-          <h4>支付方式</h4>
-          <ul class="footerItemCon">
-            <li>货到付款</li>
-            <li>在线支付</li>
-            <li>分期付款</li>
-            <li>邮局汇款</li>
-            <li>公司转账</li>
-          </ul>
-        </div>
-        <div class="footerItem">
-          <h4>售后服务</h4>
-          <ul class="footerItemCon">
-            <li>售后政策</li>
-            <li>价格保护</li>
-            <li>退款说明</li>
-            <li>返修/退换货</li>
-            <li>取消订单</li>
-          </ul>
-        </div>
-        <div class="footerItem">
-          <h4>特色服务</h4>
-          <ul class="footerItemCon">
-            <li>夺宝岛</li>
-            <li>DIY装机</li>
-            <li>延保服务</li>
-            <li>尚品汇E卡</li>
-            <li>尚品汇通信</li>
-          </ul>
-        </div>
-      </div>
       <div class="copyright">
         <ul class="helpLink">
           <li>
@@ -106,6 +54,7 @@
 
 <style lang="less">
   .footer {
+    margin-top: 50px;
     background-color: #eaeaea;
 
     .footer-container {
